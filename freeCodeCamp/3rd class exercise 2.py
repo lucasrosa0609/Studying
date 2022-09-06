e1de8f8e34b440ea322a62784cff149d53a0b336
lucasrosa0609 <lusofero@gmail.com>
2022-09-06 05:56:14 -0300

    studying

---
+++ freeCodeCamp/3rd class exercise 2.py
@@ -0,0 +1,8 @@
+# write a program to prompt the user for hours and rate per hour to compute gross pay
+
+enter_hours = input('Enter Hours: ')
+enter_rate = input('Enteder Rate: ')
+
+pay = float(enter_hours) * float(enter_rate)
+print(pay)
+
