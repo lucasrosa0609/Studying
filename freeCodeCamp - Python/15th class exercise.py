4b78775a67170f0c1f9894811121563ecf39fd76
lucasrosa0609 <lusofero@gmail.com>
2022-09-09 09:45:30 -0300

    Exercise

--- freeCodeCamp - Python/15th class exercise.py
+++ freeCodeCamp - Python/15th class exercise.py
@@ -1,6 +1,6 @@
 # write a program to read through a file and print the contents
 # of the file (line by line) all in upper case.
-# the file can be found on https://www.py4e.com/code3/mbox-short.txt
+# the file can be any file that is .txt
 
 # file_name = input('Enter the file name: ')
 file_name = input('Enter file name: ')
